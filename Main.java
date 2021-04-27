82b79b23319ffb2a7a9d1f740b7c1b0808457570
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-04-28 01:48:17 +0200

    TestApp

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean compare = DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123);
+        System.out.println(compare);
+    }
+}
